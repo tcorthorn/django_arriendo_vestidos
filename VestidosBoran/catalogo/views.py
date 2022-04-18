79f17255e744f845c752dec6f03a806a0f6ef3e7
Thomas <t.corthorn@gmail.com>
2022-04-18 19:23:05 -0400

    primeras vistas

--- VestidosBoran/catalogo/views.py
+++ VestidosBoran/catalogo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Proveedor, Categoria, Vestido, Arriendo_vestido, Cliente
+
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_vestidos=Vestido.objects.all().count()
+    num_solicitudes=Arriendo_vestido.objects.all().count()
+    # Libros disponibles (status = 'a')
+    num_solicitudes_disponibles=Arriendo_vestido.objects.filter(status__exact='a').count()
+    num_proveedores=Proveedor.objects.count()  # El 'all()' esta implícito por defecto.
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_vestidos':num_vestidos,'num_solicitudes':num_solicitudes,'num_solicitudes_disponibles':num_solicitudes_disponibles,'num_proveedores':num_proveedores},
+    )
